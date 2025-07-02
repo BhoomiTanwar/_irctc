aa634a1b9616756ce46fb759becdcfb3e64feef5
Bhoomi <100441780+BhoomiTanwar@users.noreply.github.com>
2025-07-02 22:12:40 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+///Users/bhoomi/Downloads/demo/src/main/java/com
